d3a721f2d1a4851335c563ad89eeac1e70f6c83a
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-11 15:33:48 +0300

    fixing mistake from first searching version

--- movies sql/index.sql
+++ movies sql/index.sql
@@ -1,4 +1,4 @@
-SELECT movies.name ,min(movies.`earnings-rank`)
+SELECT movies.name, movies.`earnings-rank`
 from movies
-inner join oscars on movies.id= oscars.movies_id
-WHERE oscars.type="best-picture";
+inner join oscars on movies.id= oscars.movies_id 
+WHERE oscars.type="best-picture" and movies.`earnings-rank`= (SELECT MIN(movies.`earnings-rank`) FROM movies) ;
