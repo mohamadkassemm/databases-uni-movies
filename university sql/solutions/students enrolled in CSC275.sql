dcb1b82292c0d41aa3d0b44b20e5ddb2bfc30f22
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-11 21:57:29 +0300

    CSC275 editing

--- university sql/solutions/students enrolled in CSC275.sql
+++ university sql/solutions/students enrolled in CSC275.sql
@@ -1,3 +1,4 @@
-SELECT COUNT(students_id)
-FROM enrolled
-WHERE enrolled.courses_crn=3; 
+SELECT COUNT(id)
+from courses ,enrolled,majorsin,departments
+WHERE courses.name="CSC275" and enrolled.students_id=majorsin.students_id and majorsin.departments_id=departments.id and departments.name="CS" 
+GROUP BY departments.id
